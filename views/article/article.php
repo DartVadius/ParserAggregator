30342bac959e2a26faba14c4a132a9b4aa1ddc17
DartVadius <vad261275@gmail.com>
2017-01-19 21:49:25 +0200

    view for article updated

--- views/article/article.php
+++ views/article/article.php
@@ -5,10 +5,16 @@
         </div>
         <div class="panel-body">
             <?php foreach ($img as $image): ?>            
-            <img src="<?php echo $image->link_to_image; ?>">
-
+                <img src="<?php echo $image->link_to_image; ?>">
             <?php endforeach ?>
             <?php echo $article->text ?>
+            <br>
+            <h3>А тут у нас теги</h3>
+            <p><b>
+                    <?php foreach ($tags as $tag): ?>            
+                        <?php echo $tag['tag'] . " | "; ?>
+                    <?php endforeach ?>
+                </b></p>
         </div>
         <div><a href="<?= \yii\helpers\Url::to(['site/index']); ?>">Вернуться</a></div>
     </div>
