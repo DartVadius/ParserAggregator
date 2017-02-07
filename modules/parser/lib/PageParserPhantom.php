306be6ca8449ef4bb56287758c8906254dae490c
DartVadius <vad261275@gmail.com>
2017-02-07 21:32:43 +0200

    add proxy

--- modules/parser/lib/PageParserPhantom.php
+++ modules/parser/lib/PageParserPhantom.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace app\modules\parser\lib;
+
 use PhantomInstaller\PhantomBinary;
 
 /**
@@ -9,6 +10,7 @@ use PhantomInstaller\PhantomBinary;
  * @author DartVadius
  */
 class PageParserPhantom extends PageParser {
+
     /**
      * getting page content by PhantomJS browser
      * 
@@ -17,8 +19,25 @@ class PageParserPhantom extends PageParser {
     public function __construct($url) {
         parent::__construct($url);
         $bin = PhantomBinary::getBin();
+        $proxy = ['186.219.36.19:8080',
+            '64.20.74.24:45554',
+            '62.37.237.101:8080',
+            '67.149.217.254:10200',
+            '203.77.251.162:8080',
+            '31.208.48.230:45554',
+            '47.88.188.1:3128',
+            '180.234.206.77:8080',
+            '138.68.140.197:3128',
+            '78.11.85.13:8080',
+            '188.40.37.248:3128',
+            '139.59.17.113:8080',
+            '191.179.147.46:11421',
+            '113.53.159.111:8080',
+            '68.234.205.173:45554'
+        ];        
+        $newProxy = '--proxy='.$proxy[$n];       
         $jsPath = $this->appConfig['jsSetup']['jsPath'];
-        $this->body = shell_exec("$bin $jsPath $url");        
+        $this->body = shell_exec("$bin $newProxy $jsPath $url");
     }
 
 }
